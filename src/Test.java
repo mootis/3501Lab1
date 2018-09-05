b19abb9fa9b6a135871c226feda2fee0f40d4b76
welch298 <welch298@morris.umn.edu>
2018-09-05 17:18:19 -0500

    Added comments and created README

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,8 @@
-//This is a java program to perform all permutation of given list of numbers of a specific length
 import java.util.Scanner;
 
 public class Test
 {
-
+    // Method to move the numbers around - shell sort
     static void permute(int[] a, int k)
     {
         if (k == a.length)
@@ -31,6 +30,7 @@ public class Test
         }
     }
 
+    // Generates correct number of lines based on a user input "n"
     static int factorial(int n)
     {
         if (n <= 1)
@@ -41,27 +41,35 @@ public class Test
 
     public static void main(String args[])
     {
+        // User input for "n"
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of list: ");
         int n = sc.nextInt();
         int[] sequence = new int[n];
 
+        // Begin timer for efficiency calculations
         long timeBefore = System.currentTimeMillis();
 
+        // Populates array with each combo
         for (int i = 0; i < n; i++)
             sequence[i] = (factorial(i+1));
 
+        // Displays numbers to be sorted
         System.out.println("The original sequence is: ");
         for (int i = 0; i < n; i++)
             System.out.print(sequence[i] + " ");
 
-
+        // Lists all sorted lines
         System.out.println("\nThe permuted sequences are: ");
         permute(sequence, 0);
 
+        // Ends timer for efficiency calculations
         long timeAfter = System.currentTimeMillis();
+
+        // Time to compute output
         System.out.println("Computation time: " + (timeAfter - timeBefore) + "ms");
 
+        // Ends program
         sc.close();
     }
 }
